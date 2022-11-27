d91562e86f78d8627a4957fff7b98c9a61c7a649
irante <irante@hotmail.com>
2022-11-27 20:07:57 -0300

    tercera prueba

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ let precioGigabyteG24f = 84999;
 let valorCarrito = 0;
 let comprasConfirmadas = 0;
 let agregarProducto = 0;
+let cantidad = 0;
+let confirmar = 0;
 
  function elegirOpcion (){
     opcion = parseInt(prompt ("Valor del Carrito: $" + valorCarrito + "\n" + "Articulos Confirmados: " + comprasConfirmadas + "\n" +  "Agregar al carrito: presione el numero para seleccionar el producto\n \n 1 - Pc Bangho Gm220t\n 2 - Pc Bangho Cambridgr A220\n 3- Monitor Gigabyte G24\n 4 - para salir"));
@@ -19,9 +21,9 @@ elegirOpcion();
 while (opcion != 4){
     switch (opcion) {
         case 1:
-            let cantidad = parseInt(prompt("el valor de Pc Bangho Gm220t es de $ " + precioBanghoGm220t + "\n" + "Ingrese la cantidad de unidades que desea adquirir"));
+            cantidad = parseInt(prompt("El valor de Pc Bangho Gm220t es de $ " + precioBanghoGm220t + "\n" + "Ingrese la cantidad de unidades que desea adquirir"));
            
-            let confirmar = prompt ("el valor de su compra es: $ " + precioBanghoGm220t* cantidad + "\n" + "Ingrese si para confirmar, no para salir");
+            confirmar = prompt ("El valor total de este item es: $ " + precioBanghoGm220t* cantidad + "\n" + "Ingrese si para confirmar, no para salir");
 
             if(confirmar =="si"){
                 valorCarrito = valorCarrito + (precioBanghoGm220t * cantidad);
@@ -31,9 +33,39 @@ while (opcion != 4){
                 
             break;
 
+            //*************************************************************************************** 
 
-         default:
-                alert("opcion incorrecta");
+        case 2:
+            cantidad = parseInt(prompt("El valor de Pc Bangho Cambridge A220 es de $ " + precioBanghoCambridgeA220 + "\n" + "Ingrese la cantidad de unidades que desea adquirir"));
+           
+            confirmar = prompt ("El valor total de este item es: $ " + precioBanghoCambridgeA220 * cantidad + "\n" + "Ingrese si para confirmar, no para salir");
+
+            if(confirmar =="si"){
+                valorCarrito = valorCarrito + (precioBanghoCambridgeA220 * cantidad);
+                comprasConfirmadas++;
+                
+            }
+                
+            break;
+            
+            //************************************************************************************* *
+        case 3:
+            cantidad = parseInt(prompt("El valor de Monitor Gigabyte G24f es de $ " + precioGigabyteG24f + "\n" + "Ingrese la cantidad de unidades que desea adquirir"));
+               
+            confirmar = prompt ("El valor total de este item es: $ " + precioGigabyteG24f * cantidad + "\n" + "Ingrese si para confirmar, no para salir");
+    
+            if(confirmar =="si"){
+                valorCarrito = valorCarrito + (precioGigabyteG24f * cantidad);
+                comprasConfirmadas++;
+                    
+                }
+                    
+            break;   
+
+
+
+            default:
+                alert("Opcion incorrecta !");
                 break;
 
 
@@ -42,33 +74,27 @@ while (opcion != 4){
 
 
     if (comprasConfirmadas >=2){
-        alert("gracias por Comprar, El valor Total de su compra es: $" + valorCarrito);    // si las compras son dos finaliza el bucle
+        alert("Podes comprar hasta dos producto en un mismo Carrito!\n" + "Gracias por tu Compra, El valor Total de la misma es: $" + valorCarrito);    // si las compras son dos finaliza el bucle
         break;
 
-    } else if(comprasConfirmadas > 0 && comprasConfirmadas < 2){                      // si hay compras confirmadas
-                 agregarProducto = prompt ("desea Agregar otro producto al Carrito si / no");    // pregunta si quiere realizar otra compra
-                 switch(agregarProducto){
-                    case "no":
-                         alert("visualizar el valor de la compra");
-                         break;
-
-                    case "si":
-                        elegirOpcion();
-                        break;
-                        
-                    default:
-                        alert("opcion incorrecta");
-                        break;
-
-                 }
-                
-                 
+    } else if(comprasConfirmadas > 0 && comprasConfirmadas < 2){                                        // si hay compras confirmadas
+                 agregarProducto = prompt ("Desea Agregar otro producto al Carrito? si / no");         // pregunta si quiere realizar otra compra
+                if (agregarProducto =="no") {
+                    alert ("El valor Total de su compra es de $ " + valorCarrito);                      // si no quiere seguir comprando termina el bucle
+                    break;
+                }else{
+                    elegirOpcion()
+                }
                
-             } else {
-                elegirOpcion();
-            }
-
-
+            }else {
+                elegirOpcion(); // si hay cero compras confirmadas.
+            }   
+               
+               
+               
+      
+                 
+         
          
            
 }           
