4040a30119fe96f01d0ac4825890d1c4ae0a86f4
irante <irante@hotmail.com>
2022-11-27 18:39:24 -0300

    prueba2

--- main.js
+++ main.js
@@ -9,8 +9,13 @@ let valorCarrito = 0;
 let comprasConfirmadas = 0;
 let agregarProducto = 0;
 
+ function elegirOpcion (){
+    opcion = parseInt(prompt ("Valor del Carrito: $" + valorCarrito + "\n" + "Articulos Confirmados: " + comprasConfirmadas + "\n" +  "Agregar al carrito: presione el numero para seleccionar el producto\n \n 1 - Pc Bangho Gm220t\n 2 - Pc Bangho Cambridgr A220\n 3- Monitor Gigabyte G24\n 4 - para salir"));
 
-opcion = parseInt(prompt ("Valor del Carrito: $" + valorCarrito + "\n" + "Articulos Confirmados: " + comprasConfirmadas + "\n" +  "Agregar al carrito: presione el numero para seleccionar el producto\n \n 1 - Pc Bangho Gm220t\n 2 - Pc Bangho Cambridgr A220\n 3- Monitor Gigabyte G24\n 4 - para salir"));
+ }
+
+
+elegirOpcion();
 while (opcion != 4){
     switch (opcion) {
         case 1:
@@ -31,23 +36,60 @@ while (opcion != 4){
                 alert("opcion incorrecta");
                 break;
 
-    }
-
-    if(comprasConfirmadas > 0 && comprasConfirmadas < 2){                      // si hay compras confirmadas
-        agregarProducto = prompt ("desea Agregar otro producto al Carrito si / no");    // pregunta si quiere realizar otra compra
 
     }
 
-    if(agregarProducto == "no"){
-        alert("visualizar el valor de la compra");
+
+
+    if (comprasConfirmadas >=2){
+        alert("gracias por Comprar, El valor Total de su compra es: $" + valorCarrito);    // si las compras son dos finaliza el bucle
         break;
-    }
+
+    } else if(comprasConfirmadas > 0 && comprasConfirmadas < 2){                      // si hay compras confirmadas
+                 agregarProducto = prompt ("desea Agregar otro producto al Carrito si / no");    // pregunta si quiere realizar otra compra
+                 switch(agregarProducto){
+                    case "no":
+                         alert("visualizar el valor de la compra");
+                         break;
+
+                    case "si":
+                        elegirOpcion();
+                        break;
+                        
+                    default:
+                        alert("opcion incorrecta");
+                        break;
+
+                 }
+                
+                 
+               
+             } else {
+                elegirOpcion();
+            }
+
+
+         
+           
+}           
+
+
+    
+
+
+   
+       
+       
+       
+        
 
 
 
 
-    opcion = parseInt(prompt ("Valor del Carrito: $" + valorCarrito + "\n" + "Articulos Confirmados: " + comprasConfirmadas + "\n" +  "Agregar al carrito: presione el numero para seleccionar el producto\n \n 1 - Pc Bangho Gm220t\n 2 - Pc Bangho Cambridgr A220\n 3- Monitor Gigabyte G24\n 4 - para salir"));
+    
+
+   
 
 
-}
+
 
